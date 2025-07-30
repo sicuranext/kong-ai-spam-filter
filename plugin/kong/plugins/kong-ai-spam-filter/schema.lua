2e1079cc1a6c0983de9ec6aed5ba66af4e379a4f
themiddleblue <themiddle@rev3rse.it>
2025-07-30 17:14:51 +0200

    fix: remove default value from custom response header

--- plugin/kong/plugins/kong-ai-spam-filter/schema.lua
+++ plugin/kong/plugins/kong-ai-spam-filter/schema.lua
@@ -138,7 +138,7 @@ local schema = {
           { custom_response_body = { type = "string", default = default_response_body, description = custom_response_body_description } },
           { custom_response_content_type = { type = "string", default = "application/json", description = custom_response_content_type_description } },
           { custom_response_cache_control = { type = "string", default = "max-age=0, private, no-store, no-cache, must-revalidate", description = custom_response_cache_control_description } },
-          { custom_response_header = { type = "string", default = "", description = custom_response_header_description } },
+          { custom_response_header = { type = "string", description = custom_response_header_description } },
           { debug = { type = "boolean", default = false, description = debug_description } },
         },
         entity_checks = {
